d8217f7e90b7246f5192dc129bb6c9f5dbf0ee9b
Atsushi-Endo <ena-choco@live.jp>
2020-09-15 15:50:36 +0900

    準備

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        items:null,
+        keyword:'',
+        message:''
+    },
+    watch:{
+        
+    },
+    created: function(){
+
+    },
+    methods: {
+
+    }
+})
